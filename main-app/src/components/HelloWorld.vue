0f0a838eb89c371f798148ae484156f2e6ef0579
jhinzhou <windbell520gxq@gmail.com>
2022-11-29 22:59:50 +0800

    feat: init

---
+++ main-app/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <div style="width:200px;height:200px;">
+    <h1>主应用</h1>
+    <WujieVue name="micro1" url="http://127.0.0.1:5174" style="width:100%;height:100%;"></WujieVue>
+    <WujieVue name="micro2" url="http://127.0.0.1:5175" style="width:100%;height:100%;"></WujieVue>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ micro2/src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <h1>应用2</h1>
+</template>
+
+<style scoped>
+
+</style>
