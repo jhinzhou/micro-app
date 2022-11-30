b4f5a70e48de1815531168ea55968d0a000ad83d
jhinZhou <windbell520gxq@gmail.com>
2022-11-30 16:22:09 +0800

    add: add demo

--- micro2/src/App.vue
+++ micro2/src/App.vue
@@ -1,12 +1,31 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div class="title">MICRO-TWO</div>
+  <div style="font-weight: bold; line-height: 38px">
+    {{ 'micro2.count:' + count }}
+  </div>
+  <div><button @click="change1">修改 micro1.number 的值为 20</button></div>
+  <div><button @click="changeMain">修改 main.tag 的值为 55</button></div>
 </template>
-
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+const count = ref(2)
+const change1 = () => {
+  window.$wujie.bus.$emit('change1', 20)
+}
+const changeMain = () => {
+  window.$wujie.bus.$emit('changeMain', 55)
+}
+onMounted(() => {
+  window.$wujie.bus.$on('change2', (val) => {
+    count.value = val
+  })
+})
+onUnmounted(() => {
+  window.$wujie.bus.$off('change2')
+})
+</script>
 <style scoped>
+.title {
+  color: aqua;
+}
 </style>
